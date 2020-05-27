695749048b55a61cccaef681e521aeb95334ed78
Chris Main <cbm19e@my.fsu.edu>
2020-05-27 12:56:58 -0500

    Finish Challenge 2

--- challenge 2/static/js/script.js
+++ challenge 2/static/js/script.js
@@ -1,67 +1,16 @@
-function processTime (){
-	time = prompt('Enter time in this format: "10:30 pm"');
-	time1 = time.split(' ');
-	time2 = time1[0].split(':');
-	hour = Number(time2[0]);
-	minute = Number(time2[1]);
-	if (time1[1] == "pm"){
-		hour += 12;
-	} else if (time1[1] != "am"){
-		return 'improper format';
-	}
-	minute += hour * 60;
-	return minute;
+function kittyMake (size) {
+	img = document.createElement('img');
+		// I originially did it this way, which is also correct but less parsimonious.
+		//img.setAttribute('src', 'http://thecatapi.com/api/images/get?format=src&type=gif&size=small');
+	div = document.getElementById('kitty-box'); 
+	img.src = 'http://thecatapi.com/api/images/get?format=src&type=gif&size=small';
+	img.setAttribute('alt',"luggadat kitty!");
+	img.setAttribute('width',size);
+	img.setAttribute('height',size);
+	img.setAttribute('id','007');
+	document.getElementById('kitty-box').appendChild(img);
 }
 
-
-/* This uses a case and switch to put a name 
-to the time of day that was entered above.
-*/
-function timeOfDay (minute) {
-	switch ((minute - (minute % 120))/120){
-		case 0:									// This is not an accident. It applies the same
-		case 1:									// result to cases 0 and 1.
-		result = 'wee hours';
-		break;
-		case 2:
-		result = 'crack of dawn';
-		break;
-		case 3:
-		result = 'early morning';
-		break;
-		case 4:
-		result = 'mid morning';
-		break;
-		case 5:
-		result = 'late morning';
-		break;
-		case 6:
-		result = 'early afternoon';
-		break;
-		case 7:
-		result = 'late afternoon';
-		break;
-		case 8:
-		result = 'early evening';
-		break;
-		case 9:
-		result = 'late evening';
-		break;
-		case 10:
-		result = 'early night';
-		break;
-		case 11:
-		result = 'late night';
-		break;
-	}
-	var h1 = document.createElement('h1');
-	var textAnswer = document.createTextNode("This time is the " + result + ".");
-	h1.setAttribute('id','result');
-	h1.appendChild(textAnswer);
-	document.getElementById('flex-box-result').appendChild(h1);
-
-}
-
-function clearResult () {
-	document.getElementById('result').remove();
+function byeKitty () {
+	document.getElementById('007').remove();
 }
