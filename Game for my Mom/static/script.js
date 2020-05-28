8a2e0e354a996edfc6fbabc156abc2b3e17127f9
Chris Main <cbm19e@my.fsu.edu>
2020-05-28 09:52:07 -0500

    Tiles
    I've added the tiles as objects. Next I need them to adjust images each day on the basis of this.plant and this.grass variables.

--- Game for my Mom/static/script.js
+++ Game for my Mom/static/script.js
@@ -7,7 +7,48 @@ watering = false;
 maxEndurance = 10;
 endurance = maxEndurance;
 overworked = 0;
+dayNumber = 0;
 
+function tile (num) {
+	this.id = String(num);
+	this.grass = 4;				// This will count the days since grass was last killed. 
+	this.plant = 0;				// Plant levels: 0 = no plant, 1 = seed, 2 = sprout, 3 = growing, 4 = producing, 5 = barren,
+	this.water = 2;				// Water levels: 0 = arid, 1 = dry, 2 = moderate, 3 = moist, 4 = flooded
+}
+
+tile.prototype.growGrass = function () {
+	if (this.grass < 4) {
+		this.grass ++;
+	}
+	// Add a conditional branch which adjusts the image according to grass and plant.
+}
+
+// Generate all 25 tiles as objects.
+tile1 = new tile(1)
+tile2 = new tile(2)
+tile3 = new tile(3)
+tile4 = new tile(4)
+tile5 = new tile(5)
+tile6 = new tile(6)
+tile7 = new tile(7)
+tile8 = new tile(8)
+tile9 = new tile(9)
+tile10 = new tile(10)
+tile11 = new tile(11)
+tile12 = new tile(12)
+tile13 = new tile(13)
+tile14 = new tile(14)
+tile15 = new tile(15)
+tile16 = new tile(16)
+tile17 = new tile(17)
+tile18 = new tile(18)
+tile19 = new tile(19)
+tile20 = new tile(20)
+tile21 = new tile(21)
+tile22 = new tile(22)
+tile23 = new tile(23)
+tile24 = new tile(24)
+tile25 = new tile(25)
 
 // Turn off all modes before switching to another
 function modesOff() {
@@ -20,14 +61,14 @@ function modesOff() {
 
 // Start tilling
 function tillMode() {
-	modesOff();
-	try {
+	modesOff(); 													// Turn off other modes
+	try {															// Clear last action message
 		document.getElementById('action-message').remove();
-	} catch (error) {
-	}
+	} catch (error) {												// If there wasn't a last action message,
+	}																// I don't care. Just don't bother me about it.
 	message = document.createTextNode("You put everything else away and pulled out the tiller.");
-	tilling = true;
-	h3 = document.createElement('h3');
+	tilling = true;													// Active till mode
+	h3 = document.createElement('h3');								// The next 4 lines post the message
 	h3.appendChild(message);
 	h3.setAttribute('id','action-message');
 	document.getElementById('message-box').appendChild(h3);
@@ -48,8 +89,8 @@ function plantMode() {
 	document.getElementById('message-box').appendChild(h3);
 }
 
-// Start picking weeds
-function pickMode() {
+// Start weeding
+function weedMode() {
 	modesOff();
 	try {
 		document.getElementById('action-message').remove();
@@ -97,6 +138,7 @@ function waterMode() {
 function newDay(){
 	modesOff();
 	try {
+		document.getElementById('endurance-message').remove();
 		document.getElementById('action-message').remove();
 		document.getElementById('overexertion-message').remove();
 	} catch (error) {
@@ -125,11 +167,13 @@ function newDay(){
 		maxEndurance ++;
 		endurance = maxEndurance;
 	}
+	dayNumber ++;
 }
 
 // Process button click
 function action(tileID) {
 	try {
+		document.getElementById('action-message').remove();
 		document.getElementById('endurance-message').remove();
 	} catch (error) {
 	}
@@ -144,10 +188,22 @@ function action(tileID) {
 			document.getElementById(String(tileID)).src = 'static/images/soil.jpg';
 			endurance -= 1;
 			h3 = document.createElement('h3');
+			h3.appendChild(document.createTextNode('You have tilled a plot.'));
+			h3.setAttribute('id','action-message');
+			document.getElementById('message-box').appendChild(h3);
+			h3 = document.createElement('h3');
 			h3.appendChild(describeEndurance());
 			h3.setAttribute('id','endurance-message');
 			document.getElementById('message-box').appendChild(h3);
-	}
+		}
+		if (planting) {
+		}
+		if (watering) {
+		}
+		if (harvesting) {
+		}
+		if (weeding) {
+		}
 	}
 }
 
